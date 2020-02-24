aeb608a928a6a6d6a85a6c2d2144d9e011f9d269
Abhyuday Pandey <abhyuday@iitk.ac.in>
2020-02-24 11:51:43 +0530

    Data loaded

--- solver.py
+++ solver.py
@@ -1,8 +1,69 @@
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
+import numpy as np
+
+def checkifnan(num):
+	if num is np.nan:
+		return 0
+	else:
+		return num
+
+def find3factors(time_row):
+	
+
 
 df = pd.read_excel('CE_Europe.xlsx')
+columns = list(df.loc[2])[1:]
+# First five rows have no data
+row_count = df.shape[0] - 5
+companies = dict()
+j = 0
+company_name = ""
+prev_err = False
+for i,col in enumerate(columns):
+	# Calculate the actual offset
+	idx = i + 1
+	if(col == "#ERROR"):
+		prev_err = True
+		j = (j+1)%3
+		continue
+	elif(j == 0):
+		# New Company
+		company_name = col.split('-')[0].strip()
+		companies[company_name] = dict()
+		# CAP
+		a = np.nan_to_num(np.array(df.iloc[4:(4+row_count),idx]).astype(float))
+		# print(a)
+		companies[company_name]['CAP'] = a
+		
+	elif(j == 1):
+		# ROI
+		if(prev_err == True):
+			company_name = col.split('-')[0].strip()
+			if(company_name not in companies):
+				companies[company_name] = dict()
+		a = np.nan_to_num(np.array(df.iloc[4:(4+row_count),idx]).astype(float))
+		companies[company_name]['ROI'] = a
+	elif(j == 2):
+		# PI
+		if(prev_err == True):
+			company_name = col.split('-')[0].strip()
+			if(company_name not in companies):
+				companies[company_name] = dict()
+		a = np.nan_to_num(np.array(df.iloc[4:(4+row_count),idx]).astype(float))
+		companies[company_name]['PI'] = a
 
-print("Column headings:")
-print(df.columns)
+	j = (j+1)%3
+	prev_err = False
+for company_name in companies:
+	if 'PI' not in companies[company_name]:
+		companies[company_name]['PI'] = np.array([0.]*row_count)
+	if 'ROI' not in companies[company_name]:
+		companies[company_name]['ROI'] = np.array([0.]*row_count)
+	if 'CAP' not in companies[company_name]:
+		print(company_name)
+		companies[company_name]['CAP'] = np.array([0.]*row_count)
+
+# Data Loaded
+print(companies["ALCON AG"])
